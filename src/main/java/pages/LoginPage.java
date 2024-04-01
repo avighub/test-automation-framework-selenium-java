bfc96da3ec7add473a9bf1b68556ae565460d958
Avishek Behera <avishekbehera87@gmail.com>
2024-04-01 17:27:33 +0530

    fix login tests

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,92 +1,102 @@
 package pages;
 
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.LoadableComponent;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import utilities.ConfigurationManager;
 import utilities.driverutils.SeleniumUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 
 import java.time.Duration;
 
-public class LoginPage {
-  private final WebDriver DRIVER;
+public class LoginPage extends LoadableComponent<LoginPage> {
+  private final WebDriver driver;
 
-  public LoginPage(WebDriver driverParam){
-    this.DRIVER =driverParam;
+  public LoginPage(WebDriver driver) {
+    this.driver = driver;
   }
+
+  private final String loginUrl = ConfigurationManager.ENVIRONMENT_PROPERTIES.getProperty("LOGIN_URL");
 
   //Locators
   private final By USERNAME_TEXTBOX = By.id("user-name");
   private final By PASSWORD_TEXTBOX = By.id("password");
   private final By LOGIN_BUTTON = By.id("login-button");
   private final By LOGO_IMAGE = By.xpath("//*[@id='root']/div/div[1]");
-  private final By ROBOT_IMAGE = By.xpath("//*[@id='root']/div/div[1]");
   private final By VALIDATION_ERROR_MSG = By.xpath("//h3[@data-test='error']");
 
 
   //User Actions
-  public LoginPage navigateToLoginPge()
-    {
+
+  @Override
+  protected void load() {
+    driver.get(loginUrl);
+  }
+
+  @Override
+  protected void isLoaded() throws Error {
+    String currentUrl = driver.getCurrentUrl();
+    if (!currentUrl.equals(loginUrl)) {
+      throw new IllegalStateException("Login Page is not loaded");
+    }
+  }
+
+  public LoginPage navigateToLoginPge() {
     String loginUrl = ConfigurationManager.ENVIRONMENT_PROPERTIES.getProperty("LOGIN_URL");
-    System.out.println("Navigate to : " + loginUrl);
-    DRIVER.get(loginUrl);
+    driver.get(loginUrl);
     return this;
   }
 
-  public LoginPage enterUsername(String username){
-     new WebDriverWait(DRIVER, Duration.ofSeconds(30))
-            .until(ExpectedConditions.visibilityOfElementLocated(USERNAME_TEXTBOX));
+  public LoginPage enterUsername(String username) {
+    new WebDriverWait(driver, Duration.ofSeconds(30))
+      .until(ExpectedConditions.visibilityOfElementLocated(USERNAME_TEXTBOX));
 
     System.out.println("Enter Username: " + username);
-    DRIVER.findElement(USERNAME_TEXTBOX).sendKeys(username);
+    driver.findElement(USERNAME_TEXTBOX).sendKeys(username);
     return this;
   }
 
-  public LoginPage enterPassword(String password){
+  public LoginPage enterPassword(String password) {
     System.out.println("Enter password: " + password);
-    DRIVER.findElement(PASSWORD_TEXTBOX).sendKeys(password);
+    driver.findElement(PASSWORD_TEXTBOX).sendKeys(password);
     return this;
   }
 
-  public ProductsPage clickLoginButton(){
+  public ProductsPage clickLoginButton() {
     System.out.println("Click Login button");
-    DRIVER.findElement(LOGIN_BUTTON).click();
-    return new ProductsPage(DRIVER);
+    driver.findElement(LOGIN_BUTTON).click();
+    return new ProductsPage(driver);
   }
 
   //Using method composition
-  public ProductsPage loginAsUser(String username, String password){
+  public ProductsPage loginAsUser(String username, String password) {
     enterUsername(username);
     enterPassword(password);
     clickLoginButton();
-    return new ProductsPage(DRIVER);
+    return new ProductsPage(driver);
   }
 
-  public ProductsPage loginAsUser(String userRole){
-    SeleniumUtils seleniumUtils = new SeleniumUtils(DRIVER);
-    seleniumUtils.setCookie("session-username",userRole);
-    return new ProductsPage(DRIVER);
+  public ProductsPage loginAsUser(String userRole) {
+    SeleniumUtils seleniumUtils = new SeleniumUtils(driver);
+    seleniumUtils.setCookie("session-username", userRole);
+    return new ProductsPage(driver);
   }
 
-  public ProductsPage loginAsStandardUser(){
+  public ProductsPage loginAsStandardUser() {
     loginAsUser("standard_user");
-    return  new ProductsPage(DRIVER);
+    return new ProductsPage(driver);
   }
 
-  public boolean isLogoVisible(){
+  public boolean isLogoVisible() {
     System.out.println("Check for logo visibility");
-    return DRIVER.findElement(LOGO_IMAGE).isDisplayed();
-  }
-  public boolean isRobotImageVisible(){
-    System.out.println("Check for Robot Image visibility");
-    return DRIVER.findElement(ROBOT_IMAGE).isDisplayed();
+    return driver.findElement(LOGO_IMAGE).isDisplayed();
   }
 
-  public String getValidationErrorMessage(){
+  public String getValidationErrorMessage() {
     System.out.println("Check for Validation error msg");
-    return DRIVER.findElement(VALIDATION_ERROR_MSG).getText();
+    return driver.findElement(VALIDATION_ERROR_MSG).getText();
   }
+
 
 }
--- src/test/java/login/LoginPageTest.java
+++ src/test/java/login/LoginPageTest.java
@@ -4,67 +4,69 @@ import base.BaseTest;
 import com.aventstack.extentreports.Status;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
 import pages.LoginPage;
 import utilities.driverutils.SeleniumUtils;
-import static reporting.ExtentFactory.*;
-import static utilities.driverutils.DriverFactory.*;
+
+import static reporting.ExtentFactory.getExtentTest;
+import static utilities.driverutils.DriverFactory.getDriver;
 
 public class LoginPageTest extends BaseTest {
-  private LoginPageTest(){
+  private LoginPageTest() {
   }
 
   private LoginPage loginPage;
 
   @BeforeMethod
-  void preValidation()
-    {
-      loginPage = new LoginPage(getDriver());
-      loginPage.navigateToLoginPge();
-      boolean isDisplayed = loginPage.isRobotImageVisible();
-      Assert.assertTrue(isDisplayed);
-    }
+  void setupState() {
+    loginPage = new LoginPage(driver);
+//      loginPage.get();
+    loginPage.navigateToLoginPge();
+  }
 
-  @Test
-  void verifyForLogoAndRobotImageVisibility()
-    {
-      getExtentTest().log(Status.INFO,"Checking for Logo visibility");
-      boolean isLogoVisible = loginPage.isLogoVisible();
-      boolean isRobotImageVisible = loginPage.isRobotImageVisible();
+  @DataProvider(parallel = false)
+  private Object[][] getValidLoginCredentials() {
+    return new Object[][] {
+      {"standard_user", "secret_sauce"},
+      {"performance_glitch_user", "secret_sauce"},
+    };
+  }
 
-      SoftAssert softAssert = new SoftAssert();
-      softAssert.assertTrue(isLogoVisible);
-      softAssert.assertTrue(isRobotImageVisible);
-      softAssert.assertAll();
-    }
+  @DataProvider(parallel = false)
+  public static Object[][] getInvalidLoginCredentials() {
+    return new Object[][] {
+      {"standard_user", "secret_sauce_invalid1"},
+      {"performance_glitch_user", "secret_sauce_invalid2"},
+      {"performance_glitch_user2", "secret_sauce_invalid3"},
+    };
+  }
 
-  @Test( dataProvider = "getValidLoginCredentials", dataProviderClass = LoginDataProviders.class)
-  public void verifyUserIsAbleToLoginSuccessfullyUsingValidCredentials(String username, String password)
-    {
-      //Assert
-      getExtentTest().log(Status.INFO,"Checking for Products page displayed?");
-      boolean productTitleVisible = loginPage
-                      .loginAsUser(username, password)
-                      .isProductTitleVisible();
+  @Test(dataProvider = "getValidLoginCredentials")
+  public void verifyUserIsAbleToLoginSuccessfullyUsingValidCredentials(String username, String password) {
+    //Assert
+    getExtentTest().log(Status.INFO, "Checking for Products page displayed?");
+    boolean productTitleVisible = loginPage
+      .loginAsUser(username, password)
+      .isProductTitleVisible();
 
-      SeleniumUtils.setLocalStorage("key1","value1",getDriver());
-      SeleniumUtils.setSessionStorage("key1","value1",getDriver());
-      getExtentTest().log(Status.INFO,"Session storage created");
-      Assert.assertTrue(productTitleVisible);
-    }
+    SeleniumUtils.setLocalStorage("key1", "value1", getDriver());
+    SeleniumUtils.setSessionStorage("key1", "value1", getDriver());
+    getExtentTest().log(Status.INFO, "Session storage created");
+    Assert.assertTrue(productTitleVisible);
+  }
 
-  @Test(dataProvider = "getInvalidLoginCredentials", dataProviderClass = LoginDataProviders.class)
-  public void verifyValidationErrorMessageIsDisplayedIfUserEntersInvalidCredentials(String username, String password) throws InterruptedException
-    {
-      //Act
-      loginPage.loginAsUser(username,password);
-      Thread.sleep(3000);
-      //Assert
-      String expectedValidationErrorMessage = "Epic sadface: Username and password do not match any user in this service";
-      String actualValidationErrorMessage = loginPage.getValidationErrorMessage();
-      getExtentTest().log(Status.INFO,"Checking for error message displayed correctly??");
-      Assert.assertEquals(actualValidationErrorMessage, expectedValidationErrorMessage);
-    }
+  @Test(dataProvider = "getInvalidLoginCredentials")
+  public void verifyValidationErrorMessageIsDisplayedIfUserEntersInvalidCredentials(String username, String password)
+    throws InterruptedException {
+    //Act
+    loginPage.loginAsUser(username, password);
+    Thread.sleep(3000);
+    //Assert
+    String expectedValidationErrorMessage = "Epic sadface: Username and password do not match any user in this service";
+    String actualValidationErrorMessage = loginPage.getValidationErrorMessage();
+    getExtentTest().log(Status.INFO, "Checking for error message displayed correctly??");
+    Assert.assertEquals(actualValidationErrorMessage, expectedValidationErrorMessage);
+  }
 
 }
