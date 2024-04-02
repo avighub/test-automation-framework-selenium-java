54c4b68a4be1894ed544fce1c16ced98fc47a849
Avishek Behera <avishekbehera87@gmail.com>
2024-04-02 17:17:49 +0530

    update default browser to Edge

--- src/test/java/base/TestSetup.java
+++ src/test/java/base/TestSetup.java
@@ -3,11 +3,9 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Listeners;
-import reporting.ExtentListener;
 import utilities.driverutils.DriverFactory;
 
-@Listeners(ExtentListener.class)
+//@Listeners(ExtentListener.class)
 public class TestSetup {
 
   /**
--- src/test/java/login/LoginPageTest.java
+++ src/test/java/login/LoginPageTest.java
@@ -1,7 +1,6 @@
 package login;
 
 import base.TestSetup;
-import com.aventstack.extentreports.Status;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
@@ -9,7 +8,6 @@ import org.testng.annotations.Test;
 import pages.LoginPage;
 import utilities.driverutils.SeleniumUtils;
 
-import static reporting.ExtentFactory.getExtentTest;
 
 public class LoginPageTest extends TestSetup {
   private LoginPageTest() {
@@ -26,38 +24,35 @@ public class LoginPageTest extends TestSetup {
 
   @DataProvider
   private Object[][] getValidLoginCredentials() {
-    return new Object[][] {
-      {"standard_user", "secret_sauce"},
-      {"performance_glitch_user", "secret_sauce"},
+    return new Object[][]{
+            {"standard_user", "secret_sauce"},
+            {"performance_glitch_user", "secret_sauce"},
     };
   }
 
   @DataProvider
   public static Object[][] getInvalidLoginCredentials() {
-    return new Object[][] {
-      {"standard_user", "secret_sauce_invalid1"},
-      {"performance_glitch_user", "secret_sauce_invalid2"},
-      {"performance_glitch_user2", "secret_sauce_invalid3"},
+    return new Object[][]{
+            {"standard_user1", "secret_sauce_invalid1"},
+            {"performance_glitch_user", "secret_sauce_invalid2"},
     };
   }
 
   @Test(dataProvider = "getValidLoginCredentials")
   public void verifyUserIsAbleToLoginSuccessfullyUsingValidCredentials(String username, String password) {
     //Assert
-    getExtentTest().log(Status.INFO, "Checking for Products page displayed?");
     boolean productTitleVisible = loginPage
-      .loginAsUser(username, password)
-      .isProductTitleVisible();
+            .loginAsUser(username, password)
+            .isProductTitleVisible();
 
     SeleniumUtils.setLocalStorage("key1", "value1", driver);
     SeleniumUtils.setSessionStorage("key1", "value1", driver);
-    getExtentTest().log(Status.INFO, "Session storage created");
     Assert.assertTrue(productTitleVisible);
   }
 
   @Test(dataProvider = "getInvalidLoginCredentials")
   public void verifyValidationErrorMessageIsDisplayedIfUserEntersInvalidCredentials(String username, String password)
-    throws InterruptedException {
+          throws InterruptedException {
     //Act
     loginPage.loginAsUser(username, password);
     // This is intentional to make the thread wait for this scenario
@@ -65,7 +60,6 @@ public class LoginPageTest extends TestSetup {
     //Assert
     String expectedValidationErrorMessage = "Epic sadface: Username and password do not match any user in this service";
     String actualValidationErrorMessage = loginPage.getValidationErrorMessage();
-    getExtentTest().log(Status.INFO, "Checking for error message displayed correctly??");
     Assert.assertEquals(actualValidationErrorMessage, expectedValidationErrorMessage);
   }
 
